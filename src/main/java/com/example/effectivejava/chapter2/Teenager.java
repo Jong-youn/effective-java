50ab4410f28c3aa19fda026194b6967de424bbd5
jongyounlee <jayylee.dev@gmail.com>
2023-09-17 10:48:47 +0900

    item8

---
+++ src/main/java/com/example/effectivejava/chapter2/Teenager.java
@@ -0,0 +1,8 @@
+package com.example.effectivejava.chapter2;
+
+public class Teenager {
+    public static void main(String[] args) {
+        new Room(99);
+        System.out.println("방 나감");
+    }
+}
