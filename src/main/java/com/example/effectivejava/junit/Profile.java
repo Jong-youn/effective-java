c4af2f1bf2ff5c1c778e2661a92692fd9a8b282e
jongyounlee <jayylee.dev@gmail.com>
2023-10-09 15:54:28 +0900

    refactoring

--- src/main/java/com/example/effectivejava/junit/Profile.java
+++ src/main/java/com/example/effectivejava/junit/Profile.java
@@ -33,28 +33,43 @@ public class Profile {
    }
    
    public boolean matches(Criteria criteria) {
-      score = 0;
+      calculateScore(criteria);
       
-      boolean kill = false;
-      boolean anyMatches = false;
-      for (Criterion criterion: criteria) {
-         Answer answer = answers.get(
-               criterion.getAnswer().getQuestionText());
-         boolean match = 
-               criterion.getWeight() == Weight.DontCare ||
-               answer.match(criterion.getAnswer());
-         if (!match && criterion.getWeight() == Weight.MustMatch) {
-            kill = true;
-         }
-         if (match) {
+      if (doesNotMeetAnyMustMatchCriterion(criteria)) {
+         return false;
+      }
+
+      return anyMatches(criteria);
+   }
+
+   private boolean doesNotMeetAnyMustMatchCriterion(Criteria criteria) {
+      for (Criterion criterion : criteria) {
+         boolean match = criterion.matches(answerMatching(criterion));
+         if (!match && criterion.getWeight() == Weight.MustMatch)
+            return false;
+      }
+      return false;
+   }
+
+   private void calculateScore(Criteria criteria) {
+      score = 0;
+      for (Criterion criterion : criteria) {
+         if (criterion.matches(answerMatching(criterion))) {
             score += criterion.getWeight().getValue();
          }
-         anyMatches |= match;
-         // ...
       }
-      if (kill)
-         return false;
+   }
+
+   private boolean anyMatches(Criteria criteria) {
+      boolean anyMatches = false;
+      for (Criterion criterion : criteria) {
+         anyMatches |= criterion.matches(answerMatching(criterion));
+      }
       return anyMatches;
+   }
+
+   private Answer answerMatching(Criterion criterion) {
+      return answers.get(criterion.getAnswer().getQuestionText());
    }
 
    public int score() {
